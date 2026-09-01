6a2221872dbe048fe38dd72a363d191ae7bfa56d
Egor <egor.smirnov.n@gmail.com>
2026-09-01 21:55:26 +0300

    now flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Моя NixOS конфигурация";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, impermanence }: {
+    nixosConfigurations = {
+      # Имя хоста — укажи своё (то, что в /etc/hostname)
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+	  impermanence.nixosModules.impermanence
+          ./hardware-configuration.nix
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
