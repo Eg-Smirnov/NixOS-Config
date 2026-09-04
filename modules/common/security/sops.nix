c74f48c0709b1fe52083e4bdc07d5bc4d536d141
Egor Smirnov <egor.smirnov.n@gmail.com>
2026-09-04 22:49:48 +0300

    another sops fix

--- modules/common/security/sops.nix
+++ modules/common/security/sops.nix
@@ -4,7 +4,7 @@
     defaultSopsFile = ../../../secrets.yaml;
     age = {
       # Указываем системе, где искать ключ для расшифровки
-      keyFile = "~/.config/sops/age/keys.txt";
+      keyFile = "home/server/.config/sops/age/keys.txt";
       # Или, если хочешь использовать твой текущий SSH ключ, можешь попробовать:
       # sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     };
