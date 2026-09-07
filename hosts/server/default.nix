235e3adbe587a3003074b1c77e481b23b78b819f
Egor Smirnov <egor.smirnov.n@gmail.com>
2026-09-07 21:49:25 +0300

    added revers-ssh

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -4,6 +4,7 @@
 
     ../../modules/common/security/sops.nix
     ../../modules/common/security/ssh.nix
+    ../../modules/common/security/known-hosts.nix
 
     ../../modules/common/system/power.nix
     ../../modules/common/system/configuration.nix
@@ -13,6 +14,7 @@
 
     ../../modules/common/networking/networking.nix
     ../../modules/common/networking/vpn/xray.nix
+    ../../modules/common/networking/reverse-ssh.nix
 
     ../../modules/common/programs/git.nix
   ];
---
+++ modules/common/networking/reverse-ssh.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, infrastructure, ... }:
+
+{
+  sops.secrets.reverse_tunnel_private_key = {
+    owner = "root";
+    group = "root";
+    mode = "0400";
+  };
+
+  systemd.services.reverse-ssh = {
+    description = "Reverse SSH tunnel to VPS";
+
+    wantedBy = [ "multi-user.target" ];
+
+    after = [
+      "network-online.target"
+      "sops-nix.service"
+    ];
+
+    wants = [
+      "network-online.target"
+    ];
+
+    serviceConfig = {
+      Type = "simple";
+
+      ExecStart = pkgs.writeShellScript "reverse-ssh" ''
+        exec ${pkgs.openssh}/bin/ssh \
+          -N \
+          -T \
+          -o ExitOnForwardFailure=yes \
+          -o ServerAliveInterval=30 \
+          -o ServerAliveCountMax=3 \
+          -o StrictHostKeyChecking=yes \
+          -o UserKnownHostsFile=/etc/ssh/ssh_known_hosts \
+          -o IdentityFile=${config.sops.secrets.reverse_tunnel_private_key.path} \
+          -o IdentitiesOnly=yes \
+          -R 127.0.0.1:${toString infrastructure.vps.reverseTunnel.remotePort}:127.0.0.1:22 \
+          -p ${toString infrastructure.vps.sshPort} \
+          ${infrastructure.vps.reverseTunnel.user}@${infrastructure.vps.address}
+      '';
+
+      Restart = "always";
+      RestartSec = "10s";
+
+      # Туннелю не нужны никакие привилегии.
+      User = "server";
+      Group = "users";
+
+      NoNewPrivileges = true;
+      PrivateTmp = true;
+    };
+  };
+}
