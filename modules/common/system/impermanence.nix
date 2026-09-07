6990827d9864ad51a6e48b1e84b42f044b11f262
Egor Smirnov <egor.smirnov.n@gmail.com>
2026-09-07 22:57:41 +0300

    impermanence paths fix

--- modules/common/system/impermanence.nix
+++ modules/common/system/impermanence.nix
@@ -1,18 +1,21 @@
 { config, pkgs, ... }:
 {
-	environment.persistence."/persist" = {
+  environment.persistence."/persist" = {
     hideMounts = true;
+
     directories = [
       "/var/lib"
       "/etc/NetworkManager/system-connections"
     ];
+
     users.server = {
       directories = [
         ".local/share/keyrings"
       ];
+
       files = [
         ".bash_history"
-        "~/.config/sops/age/keys.txt"
+        ".config/sops/age/keys.txt"
       ];
     };
   };
