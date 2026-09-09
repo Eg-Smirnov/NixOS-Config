73f2738a43372cbed8228d7e646700b54f5db702
Egor Smirnov <egor.smirnov.n@gmail.com>
2026-09-09 11:44:18 +0300

    removed double routing in xray.nix

--- modules/common/networking/vpn/xray.nix
+++ modules/common/networking/vpn/xray.nix
@@ -181,13 +181,6 @@
         ${pkgs.iproute2}/bin/ip route replace \
           128.0.0.0/1 dev xray0
 
-        # Full tunnel для всего остального IPv4.
-        ${pkgs.iproute2}/bin/ip route replace \
-          0.0.0.0/1 dev xray0
-
-        ${pkgs.iproute2}/bin/ip route replace \
-          128.0.0.0/1 dev xray0
-
         echo "Xray routing configured:"
         echo "  LAN: 192.168.0.0/16 -> $DEFAULT_INTERFACE"
         echo "  VPN server: $VPN_SERVER_IP -> $DEFAULT_INTERFACE"
@@ -195,8 +188,6 @@
       '';
 
       ExecStop = pkgs.writeShellScript "xray-routing-stop" ''
-        ${pkgs.iproute2}/bin/ip route del 1.0.0.1/32 2>/dev/null || true
-
         ${pkgs.iproute2}/bin/ip route del 0.0.0.0/1 dev xray0 2>/dev/null || true
         ${pkgs.iproute2}/bin/ip route del 128.0.0.0/1 dev xray0 2>/dev/null || true
       '';
