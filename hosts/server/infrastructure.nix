9ebe4591970124b68464b13ee079b8296da07839
Egor <egor.smirnov.n@gmail.com>
2026-09-12 12:38:25 +0300

    fix: update VPS infrastructure

--- hosts/server/infrastructure.nix
+++ hosts/server/infrastructure.nix
@@ -1,9 +1,9 @@
 {
   vps = {
-    address = "103.71.20.103";
+    address = "104.128.142.208";
     sshPort = 22;
 
-    hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUxH6Cc7MS6Rwq8vDEjEJyPyJ/6f/XA2+PWpn8GdoNr root@ubuntu";
+    hostKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzBAQIVPdn0sILHVQpdDgTUSNtmpxOAl8XP1hWhxkfXlG7z1jP1uDN9rYrpf1QrxMKUvH95v5l/Y6HOwNXSZvFNQYp6lOrOBAO3UCr3BNdtrA2+gJs9r9Zolb0wZBzdpyaRzQlbDgxn+H1WaN0wKGxapFEdZ+oAo82+sUsbFbyXltMvLcy9Y4Cj6Vt64VODfyUrb/cZ+13DhghHbmt/GZ5Y/djLnQi3ryQ7RAll9MihDiyHP3+VUS6AmEflRDD5DJ1lAR4/Gdpc+tjdEzpN4UvsCmjSjuhTT4py6eYg5Vzrq01rvIegMfRFnUhsZLw76LKIk68+eqFybKG6vgyfVl5y8WQlurtdxHlUbOzET9hSqWuGAEMz/AqlyKqzU2Vi/qWni0cSuSWJu8AH0S1YMKWlLpeZIjwQVrLU/Qb1AVq5TkMvAtF4UmGoE1U8RfGVYXPajgfg2HGEbFUaclgaeKIAlk+jdssGijREBzD0sJIhA6e0lZUtQh6gb3bSlzmA90=";
 
     reverseTunnel = {
       user = "reverse-tunnel";
